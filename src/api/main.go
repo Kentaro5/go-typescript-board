17aa0f6d5a03e3671ec2ed0ed6bd2aee95bb4300
kentarominami <kentaro.minami@yourmystar.jp>
2021-06-19 08:55:54 +0900

    first-commit

---
+++ src/api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+type hotdog int
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	switch req.URL.Path {
+	case "/dog":
+		io.WriteString(w, "doggy doggy doggy")
+	case "/cat":
+		io.WriteString(w, "kitty kitty kitty")
+	}
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8000", d)
+}
