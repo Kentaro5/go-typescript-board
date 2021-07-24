f67c6ea2d24ad206176d6fa988be6dc42199f2ce
kentarominami <kentaro.minami@yourmystar.jp>
2021-07-24 22:04:36 +0900

    性別リストのマスターデータをシーダーで流し込む処理作成。

--- src/api/db/go.mod
+++ src/api/db/go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	bitbucket.org/liamstask/goose v0.0.0-20150115234039-8488cc47d90c // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/kylelemons/go-gypsy v1.0.0 // indirect
 	github.com/lib/pq v1.10.2 // indirect
 	github.com/mattn/go-sqlite3 v1.14.7 // indirect
--- src/api/db/main.go
+++ src/api/db/main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
-	"api/seeders"
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"api/seed"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	seeders.CustomerSeed()
+	handleArgs()
+}
+
+func handleArgs() {
+	// コマンドラインで指定された、値を使用するために呼ぶ。
+	flag.Parse()
+	// Parseされた値を配列形式で取得。
+	args := flag.Args()
+	err := godotenv.Load(fmt.Sprintf("../%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		log.Fatalf("godotenvが使用できません。godotenvをロードしてください。", err)
+	}
+
+	if len(args) >= 1 {
+		switch args[0] {
+		case "seed":
+			connString := fmt.Sprintf("%v:%v@(%v:%v)/%v", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+
+			log.Println(connString)
+			// connect DB
+			db, err := sql.Open("mysql", connString)
+			if err != nil {
+				log.Fatalf("Error opening DB: %v", err)
+			}
+			seed.Execute(db, args[1:]...)
+			os.Exit(0)
+		}
+	}
 }
