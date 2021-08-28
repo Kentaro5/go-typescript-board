21396de53ebc5a28b3e7105f76cd50f65cf8c428
kentarominami <kentaro.minami@yourmystar.jp>
2021-08-28 16:34:12 +0900

    Createメソッドの作成。

---
+++ src/api/infrastructure/userRepositopry/userRepository.go
@@ -0,0 +1,33 @@
+package userRepositopry
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+const userDbName string = "user_tbl"
+
+func Create(db *sql.DB, user User) {
+	columns := "(name, email, password, sex_code, pref_code, city_code, ward_code, created_at, updated_at)"
+
+	fmt.Println("test")
+
+	stmt, _ := db.Prepare("INSERT INTO " + userDbName + " " + columns + " VALUES (?,?,?,?,?,?,?,?,?)")
+	// execute query
+	_, err := stmt.Exec(
+		user.Name,
+		user.Email,
+		user.PasswordHash,
+		user.TokenHash,
+		user.SexCode,
+		user.PrefCode,
+		user.CityCode,
+		user.WardCode,
+		user.CreatedAt,
+		user.UpdatedAt,
+	)
+	fmt.Println("test222")
+	if err != nil {
+		panic(err)
+	}
+}
