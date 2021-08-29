64e04bd8125058e041690b6835afd82de9e96aea
kentarominami <kentaro.minami@yourmystar.jp>
2021-08-29 23:35:22 +0900

    項目追加。

--- src/api/infrastructure/userRepositopry/user.go
+++ src/api/infrastructure/userRepositopry/user.go
@@ -1,15 +1,17 @@
 package userRepositopry
 
 type User struct {
-	Name         string
-	Email        string
-	PasswordHash string
-	TokenHash    string
-	SexCode      uint8
-	PrefCode     uint32
-	CityCode     uint32
-	WardCode     uint32
-	CreatedAt    string
-	UpdatedAt    string
-	DeletedAt    *string // may be null
+	Id            int
+	Name          string
+	Email         string
+	PasswordHash  string
+	TokenHash     string
+	SexCode       uint8
+	PrefCode      uint32
+	CityCode      uint32
+	WardCode      uint32
+	RememberToken *string
+	CreatedAt     string
+	UpdatedAt     string
+	DeletedAt     *string // may be null
 }
