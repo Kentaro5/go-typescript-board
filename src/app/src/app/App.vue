0897ad0d009a0fd3a94cd515a2ee24d67aee22b6
kentarominami <kentaro.minami@yourmystar.jp>
2021-09-11 23:28:14 +0900

    ファイルの読み込みの順番変更。

--- src/app/src/app/App.vue
+++ src/app/src/app/App.vue
@@ -1,6 +1,13 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <RouterView />
 </template>
 
 <style>
