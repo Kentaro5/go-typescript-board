0f9c8044255c31345e538284ab3b01422fb19d9e
kentarominami <kentaro.minami@yourmystar.jp>
2021-09-11 23:47:52 +0900

    登録フォーム作成。

--- src/app/src/app/main.ts
+++ src/app/src/app/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from '~/App.vue'
 import { router } from './plugins/router.ts'
+import 'bulma'
 // 5. Create and mount the root instance.
 
 createApp(App)
