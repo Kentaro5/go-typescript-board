ef5c795d8f0f714e9d4997d58b43139d2079a67a
kentarominami <kentaro.minami@yourmystar.jp>
2021-09-11 23:23:47 +0900

    viteのconfig作成。

--- src/app/vite.config.ts
+++ src/app/vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
+console.log(__dirname);
+console.log(path.resolve(__dirname, 'main.ts'));
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      fileName: 'app',
+      formats: ['es'],
+      entry: path.resolve(__dirname, 'main.ts'),
+    },
+    outDir: 'dist',
+    emptyOutDir: false,
+  },
+  resolve: {
+    alias: [
+      { find: /^~/, replacement: path.resolve(__dirname) },
+      { find: /^\/~/, replacement: path.resolve(__dirname) },
+    ],
+  },
 })
