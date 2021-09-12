595f2d51ed2e98c7964e986c2f3e6d5938f16727
kentarominami <kentaro.minami@yourmystar.jp>
2021-09-12 17:01:04 +0900

    仮の登録フォームを作成。

--- src/app/src/app/pages/sigunUp/index.vue
+++ src/app/src/app/pages/sigunUp/index.vue
@@ -1,15 +1,11 @@
 <template>
     <div class="container">
-      <form class="form-horizontal" method="POST">
-
+      <form class="form-horizontal" method="POST" action="http://localhost:8000/signUp">
         <input type="hidden" name="_token">
-
         <div class="field ">
-
           <div class="notification is-danger" v-if="err">
             <strong></strong>
           </div>
-
           <label for="name" class="label">ユーザー名（半角英数字で入力してください。）</label>
           <div class="control">
             <input id="name" name="name"
@@ -17,7 +13,6 @@
                    placeholder="ユーザー名を半角英数字で入力してください。">
           </div>
         </div>
-
         <div class="field ">
           <div class="notification is-danger" v-if="err">
             <strong></strong>
@@ -27,14 +22,13 @@
             <div class="select">
               <select name="sex_code">
                 <option value="">性別を選択してください。</option>
+                <option value="0">その他</option>
               </select>
             </div>
 
           </div>
         </div>
-
         <div class="field ">
-
           <div class="notification is-danger" v-if="err">
             <strong></strong>
           </div>
@@ -45,9 +39,7 @@
                    placeholder="example@example.com">
           </div>
         </div>
-
         <div class="field ">
-
           <div class="notification is-danger" v-if="err">
             <strong></strong>
           </div>
@@ -59,67 +51,47 @@
                    >
           </div>
         </div>
-
-        <div class="field ">
-
-          <div class="notification is-danger" v-if="err">
-            <strong></strong>
-          </div>
-          <label for="password-confirmation" class="label">確認用パスワード</label>
-          <div class="control">
-            <input id="password_confirmation" name="password_confirmation"
-                   class="input"
-                   type="password"
-                   >
-          </div>
-        </div>
-
         <div class="field ">
           <div class="control">
-
             <div class="notification is-danger" v-if="err">
               <strong></strong>
             </div>
-
             <label for="pref_code" class="label">都道府県</label>
             <div class="select">
               <select name="pref_code">
                 <option value="">都道府県を選択してください</option>
+                <option value="10006">北海道</option>
               </select>
             </div>
-
           </div>
           <div class="control">
-
             <div class="notification is-danger" v-if="err">
               <strong></strong>
             </div>
-
             <label for="city_code" class="label">市</label>
             <div class="select">
               <select name="city_code">
                 <option value=""></option>
+                <option value="11002">札幌市</option>
               </select>
             </div>
-
           </div>
           <div class="control">
             <label for="ward_code" class="label">区</label>
             <div class="select">
               <select name="ward_code">
                 <option value=""></option>
+                <option value="11011">札幌市</option>
               </select>
             </div>
 
           </div>
         </div>
-
         <div class="field is-grouped">
           <div class="control">
             <button class="button is-link">登録
             </button>
           </div>
-
         </div>
       </form>
     </div>
