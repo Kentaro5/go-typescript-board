f579088b6fb7deb5766b12f4bed412b9edb54b45
kentarominami <kentaro.minami@yourmystar.jp>
2021-09-23 16:24:39 +0900

    import方法の変更。

--- src/app/src/app/main.ts
+++ src/app/src/app/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from '~/App.vue'
-import { router } from './plugins/router.ts'
+import { router } from './plugins/router'
 import 'bulma'
 // 5. Create and mount the root instance.
 
