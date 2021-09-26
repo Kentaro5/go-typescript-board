cf892a6c284456906a1458683e582ab10ac58592
kentarominami <kentaro.minami@yourmystar.jp>
2021-09-26 16:48:54 +0900

    ユーザーIDを使って、データを取得する処理の作成。

--- src/api/infrastructure/userRepositopry/userRepositopry.go
+++ src/api/infrastructure/userRepositopry/userRepositopry.go
@@ -77,3 +77,51 @@ func FetchByEmail(db *sql.DB, email string) (*User, error) {
 
 	return &user, nil
 }
+
+func FetchByUserId(db *sql.DB, userId int) (*User, error) {
+	var sql string = "SELECT * FROM " + userTableName + " WHERE id = ?"
+	stmt, err := db.Prepare(sql)
+	if err != nil {
+		return nil, err
+	}
+	// 遅延処理の実行
+	defer stmt.Close()
+	rows, err := stmt.Query(userId)
+	if err != nil {
+		return nil, err
+	}
+	// 遅延処理の実行
+	defer rows.Close()
+
+	var user User
+	for rows.Next() {
+		// Goではループの終了後に、必ずエラーチェックを行う。
+		// 全ての行が処理されるまで、ループが継続されるとは限らないため。
+		// &形式で指定する。
+		//  Scan error on column index 0, name "id": destination not a pointerのエラーが出るため
+		err := rows.Scan(
+			&user.Id,
+			&user.Name,
+			&user.Email,
+			&user.PasswordHash,
+			&user.TokenHash,
+			&user.SexCode,
+			&user.PrefCode,
+			&user.CityCode,
+			&user.WardCode,
+			&user.RememberToken,
+			&user.CreatedAt,
+			&user.UpdatedAt,
+			&user.DeletedAt,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &user, nil
+}
