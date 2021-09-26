ff2e9cfbb8b63867c3b7d7478879ec040eb834d0
kentarominami <kentaro.minami@yourmystar.jp>
2021-09-26 21:27:51 +0900

    AuthMiddlewareで不要な箇所を削除。

--- src/api/Middleware/AuthMiddleware.go
+++ src/api/Middleware/AuthMiddleware.go
@@ -42,24 +42,11 @@ func (amw *AuthMiddleware) ValidateAccessToken(next http.Handler) http.Handler {
 		amw.accessToken = reqToken
 		claims, err := utils.ValidateAccessToken(amw.accessToken)
 
-		fmt.Println("jwtToken", claims.ExpiresAt)
-		fmt.Println("Unix", time.Now().Unix())
-		// アクセストークンを新しくするかどうかのフラグをセット
-		//var refreshTokenContext context.Context = context.WithValue(r.Context(), "refreshAccessToken", false)
-		var refreshTokenContext context.Context = context.WithValue(r.Context(), "refreshAccessToken", false)
-		r = r.WithContext(refreshTokenContext)
-		// もし、トークンの有効期限が切れていたら、新しくアクセストークンを作成するようにフラグをセット
-		if claims.ExpiresAt < time.Now().Unix() {
-			refreshTokenContext = context.WithValue(r.Context(), "refreshAccessToken", true)
-			r = r.WithContext(refreshTokenContext)
-		}
-
 		if err != nil {
 			// data.ToJSON(&GenericError{Error: err.Error()}, w)
 			utils.ToJSON(&GenericResponse{Status: false, Message: "Authentication failed. Invalid token"}, w)
 			return
 		}
-
 		// r.Context().Value()でユーザーIDを取得できるようにContextにセットしておく。
 		// 参考になりそうな記事：https://medium.com/@agatan/http%E3%82%B5%E3%83%BC%E3%83%90%E3%81%A8context-context-7211433d11e6
 		ctx := context.WithValue(r.Context(), UserIDKey{}, claims.UserID)
