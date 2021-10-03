0cce1a9f6a2298423e62b4a03ed44cabda20bae0
kentarominami <kentaro.minami@yourmystar.jp>
2021-10-03 22:32:02 +0900

    ユーザーデータを取得する処理追加

---
+++ src/app/src/app/composables/user/user.ts
@@ -0,0 +1,84 @@
+import axios from "axios";
+import {onBeforeMount, readonly, ref} from "vue";
+import {checkAuth} from "../auth/auth";
+import {decodeJwt} from "../jwt/jwt";
+
+type GetUserApiResponse = {
+    config: object
+    data: {
+        user_name: string
+        email: string
+        sex: string
+        sex_code: Number
+        prefecture: string
+        prefecture_code: Number
+        city: string
+        city_code: Number
+        ward: string | null
+        war_code: Number | null
+        created_at: string
+    }
+    headers: object
+    status: Number
+    statusText: string
+}
+
+type User = {
+    name: string
+    email: string
+    sex: string
+    sexCode: Number
+    prefecture: string
+    prefectureCode: Number
+    city: string
+    cityCode: Number
+    ward: string | null
+    wardCode: Number | null
+    registeredDate: string
+}
+
+export const useUser = () => {
+    const user = ref<User | null>(null)
+    let result = ref<boolean>(false)
+    onBeforeMount(async () => {
+        checkAuth()
+        const accessJwtToken: string | null = localStorage.getItem('accessToken')
+        if (accessJwtToken === null) {
+            result.value = false
+            return
+        }
+        const accessToken = decodeJwt(accessJwtToken)
+        const userId = accessToken.UserID
+        try {
+            // Set config defaults when creating the instance
+            const instance = axios.create({
+                baseURL: 'http://localhost:8000',
+                headers: {
+                    "Content-Type": "application/json",
+                    'Authorization': 'Bearer ' + accessJwtToken,
+                },
+            });
+
+            const response:GetUserApiResponse = await instance.get('/user/' + userId)
+            user.value = {
+                name: response.data.data.user_name,
+                email: response.data.data.email,
+                sex: response.data.data.sex,
+                sexCode: response.data.data.sex_code,
+                prefecture: response.data.data.prefecture,
+                prefectureCode: response.data.data.prefecture_code,
+                city: response.data.data.city,
+                cityCode: response.data.data.city_code,
+                ward: response.data.data.ward,
+                wardCode: response.data.data.ward_code,
+                registeredDate: response.data.data.created_at,
+            }
+            result.value = true
+        } catch (error) {
+            result.value = false
+            return
+        }
+    })
+
+    return {result: readonly(result), user: readonly(user)}
+}
